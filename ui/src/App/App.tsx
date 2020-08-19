d895403402c394ed2782f3373e858047e46b2417
Bruno <bvrsilva1@gmail.com>
2020-08-19 13:24:47 -0500

    initial commit

---
+++ ui/src/App/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import 'minireset.css'
+import './App.css'
+import Screens from '../screens'
+import Pokedex from './Pokedex'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const App: React.FC = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Pokedex>{clickLink => <Screens clickLink={clickLink} />}</Pokedex>
+    </ApolloProvider>
+  )
+}
+
+export default App
